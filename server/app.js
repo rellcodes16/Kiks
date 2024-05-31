e63decac6c10db0dd749b31e00f7e7fa0d2d79c1
JustRell <devrellmara@gmail.com>
2024-05-31 03:03:17 -0700

    Cors

--- server/app.js
+++ server/app.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const morgan = require('morgan')
-const rateLimit = require('express-rate-limit')
-const helmet = require('helmet')
-const mongoSanitize = require('express-mongo-sanitize')
-const xss = require('xss-clean') 
-const hpp = require('hpp')
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const path = require('path');
+const cors = require('cors'); 
 
-const AppError = require('./utils/apiError')
-const globalErrorHandler = require('./controllers/errorController')
-const productRouter = require('./routes/productRoutes') 
-const userRouter = require('./routes/userRoutes')
-const reviewRouter = require('./routes/reviewRoutes')
-const cartRouter = require('./routes/cartRoutes')
-const paymentRouter = require('./routes/paymentRoutes')
+const AppError = require('./utils/apiError');
+const globalErrorHandler = require('./controllers/errorController');
+const productRouter = require('./routes/productRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const cartRouter = require('./routes/cartRoutes');
+const paymentRouter = require('./routes/paymentRoutes');
 const favicon = require('serve-favicon');
 
 const app = express();
@@ -21,61 +22,62 @@ const app = express();
 // Serve favicon
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
-//1. GLOBAL MIDDLEWARES
+// 1. GLOBAL MIDDLEWARES
 
-//Set security HTTP headers
-app.use(helmet())
+// Set security HTTP headers
+app.use(helmet());
 
-//Development logging
-if(!process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+// Enable CORS for all routes
+app.use(cors());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
 }
 
-//Limit requests from same API
+// Limit requests from same API
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
-    message: 'Too many request from this IP, please try again in an hour!'
-})
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
 
 app.use('/api', limiter);
 
-//Body parser, reading data from body into req.body
+// Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
 // Data sanitization against NOSQL query injection
-app.use(mongoSanitize())
+app.use(mongoSanitize());
 
 // Data sanitization against XSS
-app.use(xss())
+app.use(xss());
 
-//Prevent parameter pollution
+// Prevent parameter pollution
 
-//Test middleware
+// Test middleware
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString()
-    // console.log(req.headers)
-
+    req.requestTime = new Date().toISOString();
+    // console.log(req.headers);
     next();
-})
+});
 
 // Define the root route
 app.get('/', (req, res) => {
     res.status(200).send('Hello, World!');
 });
 
-
-//3. ROUTES
-app.use('/api/v1/products', productRouter)
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/reviews', reviewRouter)
-app.use('/api/v1/cart', cartRouter)
-app.use('/api/v1/payment', paymentRouter)
+// 3. ROUTES
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/cart', cartRouter);
+app.use('/api/v1/payment', paymentRouter);
 
 app.all('*', (req, res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
-})
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 module.exports = app;
